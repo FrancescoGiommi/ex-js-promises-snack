3a6e1739a91318928f201f43889ac6a970e86949
Francesco <you@example.com>
2025-02-26 21:45:13 +0100

    Add snack 2

--- js/main.js
+++ js/main.js
@@ -32,3 +32,32 @@ function getPost(id) {
 getPost(1)
   .then((data) => console.log(data))
   .catch((error) => console.error(error));
+
+//! Snack 2
+/* Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, genera un numero casuale tra 1 e 6. 
+
+Tuttavia, nel 20% dei casi, il dado si "incastra" e la Promise va in reject.
+
+Bonus: HOF con closure per memorizzare l'ultimo lancio
+
+Modifica la funzione in creaLanciaDado(), che restituisce una closure che memorizza l'ultimo risultato. Se il numero esce due volte di fila, stampa "Incredibile!". */
+
+function lanciaDado() {
+  const numeroDado = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const probablyFail = Math.random();
+
+      if (probablyFail < 0.2) {
+        reject(console.log("Il dado si è incastrato"));
+      } else {
+        const randomNumber = Math.floor(Math.random() * 6) + 1;
+        resolve(console.log(`è uscito il numero ${randomNumber}`));
+      }
+    }, 3000);
+  });
+  return numeroDado;
+}
+
+lanciaDado()
+  .then((messaggio) => console.log(messaggio))
+  .catch((error) => console.error(error));
